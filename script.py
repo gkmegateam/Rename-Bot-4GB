92fbd279c2131e339d19490849ba6ebd038d57e1
RakeshSR1 <rakeshxsr7@gmail.com>
2025-11-20 22:30:02 +0530

    Update script.py

--- script.py
+++ script.py
@@ -20,7 +20,7 @@ class script(object):
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
+<b>📢 Channel :</b> <a href='https://t.me/BotX_Universe_Hub'>BotX Updates</a>
 <b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
 
 <b>♻️ Bot Made By :</b> @Madflix_Bots"""
